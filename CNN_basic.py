e5e324608b6dd27484c9d59820ab650239713ca7
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2019-06-11 23:28:34 +0900

    Update CNN basic

--- CNN_basic.py
+++ CNN_basic.py
@@ -1,6 +1,9 @@
 from keras import layers
 from keras import models
 
+from keras.datasets import mnist
+from keras.utils import to_categorical
+
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)))
 model.add(layers.MaxPooling2D(2, 2))
@@ -11,4 +14,19 @@ model.add(layers.Flatten())
 model.add(layers.Dense(64, activation="relu"))
 model.add(layers.Dense(10, activation="softmax"))
 
-model.summary()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+train_images = train_images.astype("float32") / 255 # Normalization
+
+test_images = test_images.reshape((10000, 28, 28, 1))
+test_images = test_images.astype("float32") / 255 # Normalization
+
+train_labels = to_categorical(train_labels) # Change to [0, 1, 2, ..., 9]
+test_labels = to_categorical(test_labels) # Change to [0, 1, 2, ..., 9]
+
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
+model.fit(train_images, train_labels, epochs=5, batch_size=64)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print("test_acc: ", test_acc)
--- README.md
+++ README.md
@@ -7,3 +7,11 @@
 - Như ví dụ trong file **CNN_basic.py** tầng Dense cuối cùng sẽ có kích cỡ 10, vì lí do đó nó có khả năng phân loại cho 10 loại đầu ra. Mạng CNN xây dựng được sẽ có kiến trúc như sau:
 
 <img src="images/pic1.png" />
+
+### Cơ chế hoạt động
+
+- Khác với mạng neural thông thường, chỉ học các global pattern thì CNN sẽ học các local pattern của một bức ảnh (1 window của CNN có kích cỡ khá nhỏ: 2x2, 3x3)
+
+- Có 2 tính chất quan trọng của CNN mà ta cần nắm rõ:
+  - Việc CNN học các pattern là di chuyển bất biến. Nói 1 cách đơn giản đó là CNN sẽ học các pattern của 1 bức ảnh từ góc dưới bên phải cho đến góc trên bên trái. Mạng neural thông thường sẽ phải học các pattern xuất hiện ở chỗ mới thêm 1 lần nữa. Ở CNN, mạng này học được các thể hiện có tính tổng quát hoá cao nên số lượng các ví dụ đi kèm sẽ ít đi
+  - Tầng thứ nhất của CNN sẽ học những chi tiết nhỏ (ở viền - edge), tầng thứ 2 sẽ học những chi tiết lớn hơn dựa theo những đặc trưng bóc tách được từ tầng thứ nhất, nên CNN sẽ có khả năng trừu tượng hoá hoá khái niệm thị giác của bức ảnh khá cao
